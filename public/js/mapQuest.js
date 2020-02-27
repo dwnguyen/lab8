d8fb3d72d374e4b71c2eea68bd179337227d2499
David Nguyen <dwnguyen@ucsd.edu>
2020-02-27 15:05:02 -0800

    finished

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'f5PLDH0DsFfAGMJVGNW5C5mvP2VEwpbi';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88,-117.236]).addTo(map);
 }
